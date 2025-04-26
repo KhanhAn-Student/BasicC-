73e9dda97b805a45577add85fe0d52020b86077b
KhanhAn-Student <anngockhanh0145@gmail.com>
2025-04-26 11:17:12 +0700

    Update bai1.cpp

--- bai1.cpp
+++ bai1.cpp
@@ -2,11 +2,11 @@
 #include <cmath>
 using namespace std;
 
-int snt(int x)
+int snt(int x)				//hàm kiểm tra có phải số nguyên tố không
 {
     if (x < 2)
         return 0;
-    for (int i = 2; i <= sqrt(x); i++)
+    for (int i = 2; i <= sqrt(x); i++)			
         if (x%i == 0)
             return 0;
     return 1;
@@ -17,7 +17,7 @@ int main(){
     cin >> n;
     if(n<2) cout<<"Khong co so nguyen to";
     else{
-    for (int i = 1; i < n; i++){
+    for (int i = 1; i < n; i++){       //chạy từ 1-n số nào là số nguyên tố thì in ra
 		if (snt(i)) 
             cout << i << " ";
 		}    	
